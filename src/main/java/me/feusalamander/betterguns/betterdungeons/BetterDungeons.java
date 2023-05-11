7528487163dc6c65ae49f42d4f4df3b5d50be1e4
FeuSalamander <ferreresarnaud@gmail.com>
2023-05-11 16:09:52 +0200

    initial commit

---
+++ src/main/java/me/feusalamander/betterguns/betterdungeons/BetterDungeons.java
@@ -0,0 +1,17 @@
+package me.feusalamander.betterguns.betterdungeons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterDungeons extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
